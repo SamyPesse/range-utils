c652af7b48d1a00b111027985002befd97948be6
Samy Pessé <samypesse@gmail.com>
2016-04-07 14:17:04 +0200

    Add method toTree

--- test/range.js
+++ test/range.js
@@ -43,38 +43,6 @@ describe('Range', function() {
         });
     });
 
-    describe('sortByLength', function() {
-        it('should sort ranges', function() {
-            Range.sortByLength([
-                {
-                    offset: 4,
-                    length: 5
-                },
-                {
-                    offset: 0,
-                    length: 4
-                },
-                {
-                    offset: 2,
-                    length: 6
-                }
-            ]).should.deepEqual([
-                {
-                    offset: 0,
-                    length: 4
-                },
-                {
-                    offset: 4,
-                    length: 5
-                },
-                {
-                    offset: 2,
-                    length: 6
-                }
-            ]);
-        });
-    });
-
     describe('.areCollapsing', function() {
         it('should return false if not collapsing', function() {
             Range.areCollapsing({
@@ -94,77 +62,6 @@ describe('Range', function() {
                 offset: 10,
                 length: 10
             }).should.equal(true);
-        });
-    });
-
-    describe('.linearize', function() {
-        it('should not modified linearized ranges', function() {
-            var out = Range.linearize([
-                {
-                    offset: 0,
-                    length: 10,
-                    type: 'BOLD'
-                },
-                {
-                    offset: 10,
-                    length: 10,
-                    type: 'ITALIC'
-                }
-            ]);
-
-            out.should.have.lengthOf(2);
-        });
-
-        it('should linearize ranges', function() {
-            Range.linearize([
-                {
-                    offset: 0,
-                    length: 13,
-                    type: 'BOLD'
-                },
-                {
-                    offset: 10,
-                    length: 10,
-                    type: 'ITALIC'
-                }
-            ]).should.deepEqual([
-                {
-                    offset: 0,
-                    length: 10,
-                    type: 'BOLD'
-                },
-                {
-                    offset: 10,
-                    length: 3,
-                    type: 'BOLD'
-                },
-                {
-                    offset: 10,
-                    length: 3,
-                    type: 'ITALIC'
-                },
-                {
-                    offset: 13,
-                    length: 7,
-                    type: 'ITALIC'
-                }
-            ]);
-        });
-
-        it('should not linearized // ranges', function() {
-            var ranges = [
-                {
-                    offset: 0,
-                    length: 10,
-                    type: 'BOLD'
-                },
-                {
-                    offset: 0,
-                    length: 10,
-                    type: 'ITALIC'
-                }
-            ];
-            Range.linearize(ranges).should.deepEqual(ranges);
         });
     });
 
